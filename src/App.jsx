4d7d812698e28b2640ac57b2cf36b73159b7fd53
khasnhnq <quockhanh31032005@gmail.com>
2026-09-24 21:37:13 +0700

    test jenkins auto deploy

--- src/App.jsx
+++ src/App.jsx
@@ -171,7 +171,7 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th>ID</th>
+            <th>ID key</th>
             <th>Tên sản phẩm</th>
             <th>Giá</th>
             <th>Số lượng</th>
